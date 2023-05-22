2e9b2aa1e8f3dfac6e5a792aab2cfd3ef2fb94f0
Zleepless <Donutsepia@gmail.com>
2023-05-22 07:35:03 -0400

    Finishing Lab

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,41 +3,88 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
+public_users.post("/register", (req, res) => {
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    if (username && password) {
+        if (!isValid(username)) {
+            users.push({ "username": username, "password": password });
+            return res.status(200).json({ message: "User successfully registred. Now you can login" });
+        } else {
+            return res.status(404).json({ message: "User already exists!" });
+        }
+    }
+    return res.status(404).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', async function (req, res) {
+    //Write your code here
+    try {
+        res.send(JSON.stringify(books, null, 10));
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Fetching Error');
+    }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+public_users.get('/isbn/:isbn', async function (req, res) {
+    //Write your code here
+    try {
+        const isbn = req.params.isbn;
+        res.send(books[isbn])
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Fetching Error');
+    }
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    //Write your code here
+    try {
+        const author = req.params.author;
+        const matchingBooks = [];
+        for (const key in books) {
+            if (books.hasOwnProperty(key) && books[key].author === author) {
+                matchingBooks.push(books[key]);
+            }
+        }
+
+        res.send(matchingBooks);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).send('Fetching Error');
+    }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+    //Write your code here
+    const title = req.params.title;
+    const matchingTitles = [];
+
+    // Iterate through the 'books' object to find books with matching authors
+    for (const key in books) {
+        if (books.hasOwnProperty(key) && books[key].title === title) {
+            matchingTitles.push(books[key]);
+        }
+    }
+
+    res.send(matchingTitles);
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+    //Write your code here
+    const isbn = req.params.isbn;
+    res.send(books[isbn].reviews)
 });
 
 module.exports.general = public_users;
